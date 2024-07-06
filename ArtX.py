90b7deffa69721b979cda8a40a3b6f0909fefaca
Tudor Filip <tudorfilip2110@gmail.com>
2024-07-06 14:02:41 +0300

    Add files via upload

--- ArtX.py
+++ ArtX.py
@@ -1,9 +1,11 @@
 from tkinter import *
 from tkinter import ALL, colorchooser, filedialog, PhotoImage
 from PIL import *
-from PIL import Image, ImageDraw, ImageColor, ImageTk
+from PIL import Image, ImageDraw, ImageColor, ImageTk, ImageEnhance
 import os
 import random
+import pystray
+from pystray import MenuItem as item
 
 width, height = 1540, 720
 
@@ -14,27 +16,35 @@ WIN.resizable(False, False)
 WIN.config(bg="white")
 
 directories = ['ArtX', 'Assets']
-filename = 'rectangle.png'
+filename : str = 'rectangle.png'
 current_directory = os.getcwd()
 
 x0, y0, x1, y1 = None, None, None, None
 
 #liste de cuvinte pt generator
 d1 = ["blue", "red", "yellow", "green", "big", "small", "little", "long", "white", "black", "sad", "happy", "sleepy", "motorized", "tall", "smart", "huge", "clean", "dirty", "bright", "dark", "ornate", "festive", "glowing"]
-d2 = ["cat", "dog", "plane", "iguana", "fish", "bird", "car", "planet", "book", "elephant", "star", "computer", "building", "bee", "ant", "hat", "object", "lion", "shark", "ball", "tank", "ship", "trumpet", "tree"]
+d2 = ["cat", "dog", "plane", "iguana", "fish", "bird", "car", "planet", "book", "elephant", "star", "computer", "building", "bee", "ant", "hat", "object", "lion", "shark", "ball", "tank", "ship", "trumpet", "tree", "robot"]
 d3 = ["assembling", "eating", "building", "demolishing", "using", "selling", "placing", "gathering", "smelling", "tasting", "atacking", "planting", "seeing", "hiding", "burying", "decorating", "drawing"]
 d4 = ["a building", "a phone", "a home", "a guitar", "a piano", "a pizza", "a flower", "a tresure", "a letter", "an essay", "a laptop", "a tractor", "a fork", "a hat", "an entity(?)", "a fish", "a bat", "a poster", "a toy"]
 
-Colour = "#000000"
+Colour : str = "#000000"
 Colour2 = ImageColor.getrgb("black")
 
-canvas_name = "canvas"
+canvas_name : str = "canvas"
+image_states : list = []
 
-bkgr_clr = "#ffffff"
+bkgr_clr : str = "#ffffff"
 image = Image.new("RGB", (1000, 720), "#ffffff")
 draw = ImageDraw.Draw(image)
 
+brush_size : int = 10
+brush_shape : str = "ellipse"
+brush_type : str = "brush"
+entry_suggestion = Entry(WIN, width=40)
+entry_suggestion.place(x=10, y=350)
+
 #Creeaza canvasul si il face alb
+CVS_width, CVS_height = 1000, 720
 CVS = Canvas(WIN, width = 1000, height = 720) 
 CVS.create_rectangle(2, 2, 1001, 717, fill = "#ffffff", outline="#000000")
 CVS.config(cursor="pencil") 
@@ -47,11 +57,8 @@ WIN.config(menu=menu)
 bs_sub_menu = Menu(menu, tearoff=0)
 sp_sub_menu = Menu(menu, tearoff=0)
 
-brush_size = 5
-brush_shape = "ellipse"
-brush_type = "brush"
-entry_suggestion = Entry(WIN, width=40)
-entry_suggestion.place(x=10, y=350)
+filter_type = None
+brightness_factor : float = 1.0
 
 def find_artx_directory(directory): #gasete locatia folderuli ArtX
     for dirpath, dirnames, filenames in os.walk(directory):
@@ -80,12 +87,20 @@ path = path.replace('eraser.png', 'brush.png')
 photo_brush = PhotoImage(file = path) 
 path = path.replace('brush.png', 'bucket.png')
 photo_bucket = PhotoImage(file = path) 
+path = path.replace('bucket.png', 'lighten.png')
+photo_lighten = PhotoImage(file = path) 
+path = path.replace('lighten.png', 'darken.png')
+photo_darken = PhotoImage(file = path) 
+path = path.replace('darken.png', 'icon.png')
+photo_icon = PhotoImage(file = path) 
+
+WIN.iconphoto(False, photo_icon)
 
 class App:
 
     def paint(self, event ): #Functia care se activeaza cand dai click/drag ca sa pictezi
         global x0, y0, x1, y1
-        if brush_type == "brush":   #pentru pensula
+        if brush_type == "brush" or brush_type == "eraser":   #pentru pensula
             x0, y0, x1, y1 = ( event.x - brush_size/2 ),( event.y- brush_size/2), ( event.x + brush_size/2 ),( event.y + brush_size/2 )
             if brush_shape == "ellipse":
                 CVS.create_oval( x0, y0, x1, y1, fill = Colour, outline= Colour )
@@ -146,16 +161,41 @@ class App:
         x0, y0, x1, y1 = ( event.x - 20 ),( event.y - 20), ( event.x + 20 ), ( event.y + 20 )
         CLR.create_oval( x0, y0, x1, y1, fill = Colour, outline= Colour )
         
-    def pick_color(self):   #Deschide interfata de selectare a culorii
+    def pick_color(self):
         global Colour, Colour2
-        clr = colorchooser.askcolor()[1]  
+        clr = colorchooser.askcolor()[1]  # Get the hex color
         if clr:
             Colour = clr
-            Colour2 = ImageColor.getrgb(clr)
+            Colour2 = ImageColor.getrgb(clr)  # For Pillow image
+            
+            # Convert to grayscale regardless of any external conditions
+            if filter_type == "grs":
+                grayscale_value = int((0.299 * Colour2[0] + 0.587 * Colour2[1] + 0.114 * Colour2[2]))
+                Colour = f'#{grayscale_value:02x}{grayscale_value:02x}{grayscale_value:02x}'  # For Tkinter canvas
+                Colour2 = (grayscale_value)
+
+    def assign_colour(self, x):  #Functia pentru butoanele de culoare
+        global Colour, Colour2
+        Colour = x
+        CVS.config(cursor="pencil") 
+        try:
+            Colour2 = ImageColor.getrgb(x)
+            if filter_type == "grs":
+                grayscale_value = int((0.299 * Colour2[0] + 0.587 * Colour2[1] + 0.114 * Colour2[2]))
+                Colour = f'#{grayscale_value:02x}{grayscale_value:02x}{grayscale_value:02x}'  # For Tkinter canvas
+                Colour2 = (grayscale_value)
+        except ValueError:
+            Colour2 = ImageColor.getrgb("black")
+            Colour =  "#000000000"
+            if filter_type == "grs":
+                grayscale_value = int((0.299 * Colour2[0] + 0.587 * Colour2[1] + 0.114 * Colour2[2]))
+                Colour = f'#{grayscale_value:02x}{grayscale_value:02x}{grayscale_value:02x}'  # For Tkinter canvas
+                Colour2 = (grayscale_value)
 
     def set_type(self, shape_type): #Seteaza tipul de pensula (pensula, dreptunghi, elipsa, linie)
         global brush_type, Colour
         brush_type = shape_type
+        CVS.config(cursor="pencil") 
         if Colour == bkgr_clr:
             Colour = "black"
         
@@ -167,12 +207,13 @@ class App:
 
     def new_file(self): #Acelasi lucru dar da fill cu alb (si face un canvas nou in Pillow)
         global image, draw
+        CVS.config(height=CVS_height, width=CVS_width)
         CVS.create_rectangle(2, 2, 1001, 717, fill = "#ffffff", outline="#000000")
         image = Image.new("RGB", (1000, 720), "#ffffff")
         draw = ImageDraw.Draw(image)
 
     def get_idea(self): #Generator idei
-        num = random.randint(0, 1000)
+        num = random.randint(0, 100)
         w = d1[random.randint(0, len(d1) - 1)]
         x = d2[random.randint(0, len(d2) - 1)]
         y = d3[random.randint(0, len(d3) - 1)]
@@ -181,17 +222,21 @@ class App:
         idea = f"a {w} {x} {y} {z}."
         if num == 42:
             idea = "the meaning of life."
+            CVS.config(cursor="question_arrow") 
+        elif num == 53:
+            idea = "a detective and his robot partner."
+            CVS.config(cursor="man")
+        elif  num == 99:
+            idea = "a Lord using the Force."
+            CVS.config(cursor="star")
+        elif num == 79:
+            idea = "a whale, above the surface of an alien planet."
+            CVS.config(cursor="boat")
+        elif num == 66:
+            idea = "somewhere where no man has ever gone before!"
+            CVS.config(cursor="trek") 
         entry_suggestion.insert(0, idea)
         return
-
-    def assign_colour(self, x):  #Functia pentru butoanele de culoare
-        global Colour, Colour2
-        Colour = x
-        try:
-            Colour2 = ImageColor.getrgb(x)
-        except ValueError:
-            Colour2 = ImageColor.getrgb("black")
-            Colour =  "#000000000"
 
     def save_image_as(self): #Salveaza imaginea ca...
         file_path = filedialog.asksaveasfilename(title="Save Image File", defaultextension=".png", filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif *.bmp *.ico")])
@@ -199,6 +244,8 @@ class App:
             image.save(file_path)
 
     def open_image(self):  #Dialogul pentru deschis poze
+        global brightness_factor, CVS
+        brightness_factor = 1.0
         file_path = filedialog.askopenfilename(title="Open Image File", filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif *.bmp *.ico")])
         if file_path:
             self.display_image(file_path)
@@ -216,10 +263,12 @@ class App:
     def display_image(self, file_path):   #Incarca imaginea aleasa in open_image
         global image, draw, CVS, tk_image
         loaded_image = Image.open(file_path)
-        loaded_image = loaded_image.resize((1000, 720))
+        while loaded_image.width > CVS_width or loaded_image.height > CVS_height:
+            loaded_image = loaded_image.resize((loaded_image.width - loaded_image.width//10, loaded_image.height - loaded_image.height//10))
         image = loaded_image
         draw = ImageDraw.Draw(image)
         tk_image = ImageTk.PhotoImage(loaded_image)
+        CVS.config(height=image.height, width=image.width)
         CVS.delete(ALL)
         CVS.create_image(0, 0, anchor=NW, image=tk_image)
         CVS.image = tk_image
@@ -241,9 +290,60 @@ class App:
             brush_type = "brush"
 
     def eraser(self): #Guma de sters (seteaza culoarea pensulei la culoarea backgrounduli)
-        global bkgr_clr, Colour, Colour2
+        global bkgr_clr, Colour, Colour2, brush_type
+        CVS.config(cursor="circle") 
         Colour = bkgr_clr
         Colour2 = ImageColor.getrgb(Colour)
+
+    #Filtre
+    def filter(self, _type): 
+        global image, draw, tk_image, Colour, Colour2, filter_type, brightness_factor
+        if _type == "bnw":
+            image = image.convert("1")
+            Colour = 'black'
+            Colour2 = 0
+        elif _type == "grs":
+            image = image.convert("L")
+            Colour = 'black'
+            Colour2 = 0
+        elif _type == None:
+            image = image.convert("RGB")
+            Colour = 'black'
+            Colour2 = 0
+        elif _type == 'uex':
+            brightness_factor = 0.5
+            enhancer = ImageEnhance.Brightness(image)
+            image = enhancer.enhance(brightness_factor)
+            brightness_factor = 1.0
+        elif _type == 'oex':
+            brightness_factor = 1.5
+            enhancer = ImageEnhance.Brightness(image)
+            image = enhancer.enhance(brightness_factor)
+            brightness_factor = 1.0
+        filter_type = _type
+
+        draw = ImageDraw.Draw(image)
+        tk_image = ImageTk.PhotoImage(image)
+        CVS.delete(ALL)
+        CVS.create_image(0, 0, anchor=NW, image=tk_image) 
+        CVS.image = tk_image
+
+    def brightness(self, _type):
+        global brightness_factor, image, tk_image, draw
+        if _type == 0:
+            brightness_factor -= 0.1
+            enhancer = ImageEnhance.Brightness(image)
+            image = enhancer.enhance(brightness_factor)
+        elif _type == 1:
+            brightness_factor += 0.1
+            enhancer = ImageEnhance.Brightness(image)
+            image = enhancer.enhance(brightness_factor)
+        draw = ImageDraw.Draw(image)
+        tk_image = ImageTk.PhotoImage(image)
+        CVS.delete(ALL)
+        CVS.create_image(0, 0, anchor=NW, image=tk_image) 
+        CVS.image = tk_image
+        brightness_factor = 1.0
 
     #Functia care creeaza butoanele
     def draw_buttons(self):
@@ -251,20 +351,20 @@ class App:
 
         #butoane stanga
         #seteaza marimea pensulei
-        spinbox_brush_size = Spinbox(WIN, from_= 1, to=2000, width=5)
+        spinbox_brush_size = Spinbox(WIN, from_= 1, to=2000, width=5, command= lambda: self.pick_size(spinbox_brush_size.get()))
         spinbox_brush_size.place(x=2, y = 5)
         #confirma marimea 
-        button_check = Button(WIN, image=photo_check, bg='white', border=0, activebackground='white')
+        button_check = Button(WIN, image=photo_check)
         button_check.place(x=50, y=5)                                                                                   #Foloseste lambda ca sa dea call la functie doar cand apesi pe buton
-        button_check.config(width=40, height=20, cursor="hand2", command = lambda: self.pick_size(spinbox_brush_size.get())) #Altfel ii da call doar cand creaza butonul
+        button_check.config(width=28, height=20, cursor="hand2", command = lambda: self.pick_size(spinbox_brush_size.get())) #Altfel ii da call doar cand creaza butonul
         #guma
         button_eraser = Button(WIN, image=photo_eraser)
-        button_eraser.place(x=100, y=5)
-        button_eraser.config(width=60, height=20, cursor="hand2", command = self.eraser)
+        button_eraser.place(x=90, y=5)
+        button_eraser.config(width=74, height=20, cursor="hand2", command = self.eraser)
         #pensula
         button_brush = Button(WIN, image=photo_brush)
-        button_brush.place(x=170, y=5)
-        button_brush.config(width=80, height=20, cursor="hand2", command = lambda: self.set_type("brush"))
+        button_brush.place(x=176, y=5)
+        button_brush.config(width=72, height=20, cursor="hand2", command = lambda: self.set_type("brush"))
         #sugestii de desen
         button_suggestion = Button(WIN, text="Draw:")
         button_suggestion.place(x=10, y=300)
@@ -281,6 +381,14 @@ class App:
         button_ellipse = Button(WIN, image = photo_line, compound = LEFT)
         button_ellipse.place(x=175, y=35)
         button_ellipse.config(width=73, height=36, cursor="hand2", command = lambda: self.set_type("line"))
+        #darken
+        button_darken = Button(WIN, image = photo_darken, compound = LEFT)
+        button_darken.place(x=5, y=85)
+        button_darken.config(width=115, height=36, cursor="hand2", command = lambda: self.brightness(0))
+        #lighten
+        button_lighten = Button(WIN, image = photo_lighten, compound = LEFT)
+        button_lighten.place(x=133, y=85)
+        button_lighten.config(width=115, height=36, cursor="hand2", command = lambda: self.brightness(1))
 
         #butoane dreapta
         #clr/bgr
@@ -333,26 +441,33 @@ class App:
 
 
     def draw_menu(self):    #Creaza butoanele din meniu
-        filemenu = Menu(menu)
-        toolmenu = Menu(menu)
-        menu.add_cascade(label='File', menu=filemenu)
-        filemenu.add_command(label='New          Ctrl+N', command=self.new_file)
-        filemenu.add_command(label='Open...      Ctrl+O', command=self.open_image)
-        filemenu.add_command(label='Save As...   Ctrl+S', command = self.save_image_as)
-        filemenu.add_separator()
-        filemenu.add_command(label='Exit', command=WIN.quit)
+        file_menu: Menu = Menu(menu)
+        tool_menu: Menu = Menu(menu)
+        filter_menu: Menu = Menu(menu)
+        menu.add_cascade(label='File', menu=file_menu)
+        file_menu.add_command(label='New          Ctrl+N', command=self.new_file)
+        file_menu.add_command(label='Open...      Ctrl+O', command=self.open_image)
+        file_menu.add_command(label='Save As...   Ctrl+S', command = self.save_image_as)
+        file_menu.add_separator()
+        file_menu.add_command(label='Exit', command=WIN.quit)
 
-        menu.add_cascade(label='Tools', menu=toolmenu)
-        toolmenu.add_cascade(label='Brush Shape', menu=bs_sub_menu)
+        menu.add_cascade(label='Tools', menu=tool_menu)
+        tool_menu.add_cascade(label='Brush Shape', menu=bs_sub_menu)
         bs_sub_menu.add_command(label='Ellipse', command= lambda: self.pick_shape("ellipse"))
         bs_sub_menu.add_command(label='Rectangle', command= lambda: self.pick_shape("rectangle"))
         bs_sub_menu.add_command(label='Pen', command= lambda: self.pick_shape("pen"))
 
-        toolmenu.add_cascade(label="Shapes", menu=sp_sub_menu)
+        tool_menu.add_cascade(label="Shapes", menu=sp_sub_menu)
         sp_sub_menu.add_command(label='Rectangle', command = lambda: self.set_type("rectangle"))
         sp_sub_menu.add_command(label='Ellipse', command = lambda: self.set_type("ellipse"))
         sp_sub_menu.add_command(label='Line', command = lambda: self.set_type("line"))
 
+        menu.add_cascade(label='Filters', menu=filter_menu)
+        filter_menu.add_command(label='Grayscale', command= lambda: self.filter("grs"))
+        filter_menu.add_command(label='Black and White', command= lambda: self.filter("bnw"))
+        filter_menu.add_command(label='RGB', command= lambda: self.filter(None))
+        filter_menu.add_command(label='Underexpose', command= lambda: self.filter("uex"))
+        filter_menu.add_command(label='Overexpose', command= lambda: self.filter("oex"))
 
     def draw_label(self):   #Text + Notes
         label_test_clr = Label(WIN, text="Compare Colours", font=("Arial", 16, "bold"), fg="black", bg="white")
@@ -362,7 +477,7 @@ class App:
         notes = Text(WIN, width=30, height=19, bg="grey94")
         notes.place(x=10, y=400)
 
-app = App()
+app : App = App()
 print(artx_directory)
 #Eventuri
 CVS.bind( "<B1-Motion>", app.paint )
